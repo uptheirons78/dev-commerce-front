0aef6ef4547d60dae00abcfb7748f43d0812a1ca
mauro bono <bono.mauro@gmail.com>
2019-12-31 16:51:23 +0100

    Step 3: Added Sign Up on the front end with modals to show error or success message

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 * Create a Routes Component and render it instead of App;
 * Add Home Component;
 
-#### Bootstrap 4 [step-02]
+#### Layout, Menu and Bootstrap 4 [step-02]
 
 * Clean `public/index.html` and add Bootstrap 4 css via CDN;
 * Create a Menu Component with Link and withRouter();
@@ -16,3 +16,10 @@
 * Add Menu Compontent to Layout Component;
 * Import Layout Component inside existing pages;
 * Create .env to import 'secret variables' via config.js;
+
+#### Signup [step-03]
+
+* Create SignUp Form inside `Signup.js`;
+* Create `handleChange` and `clickSubmit` handlers in the same file;
+* Create `signup` method using fetch() to POST data on the backend inside `auth` folder;
+* Add `showError` and `showSuccess` methods show modals on success or show errors to the user;
--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,13 +1,91 @@
-import React from 'react';
-import { API } from '../config';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// Auth Method
+import { signup } from '../auth/index';
 
 // Components
 import Layout from '../core/Layout';
 
 const Signup = () => {
+  // Initial State
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false
+  });
+
+  const { name, email, password, success, error } = values;
+
+  // handleChange Method
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  // clickSubmit Method
+  const clickSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then(data => {
+      if (data.errors) {
+        setValues({ ...values, error: data.errors[0].msg, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: '',
+          email: '',
+          password: '',
+          error: '',
+          success: true
+        });
+      }
+    });
+  };
+
+  // Form Component
+  const SignUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Name</label>
+        <input onChange={handleChange('name')} type='text' className='form-control' value={name} />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input onChange={handleChange('email')} type='email' className='form-control' value={email} />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input onChange={handleChange('password')} type='password' className='form-control' value={password} />
+      </div>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div className='alert alert-danger' style={{ display: error ? '' : 'none' }}>
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div className='alert alert-info' style={{ display: success ? '' : 'none' }}>
+      New account is created. Please <Link to='/signin'>Sign In</Link>
+    </div>
+  );
+
   return (
-    <Layout title='SignUp Page' description='Signup: Node React Ecommerce Application'>
-      {API}
+    <Layout
+      title='Sign Up Page'
+      description='Sign Up: Node React Ecommerce Application'
+      className='container col-md-8 offset-md-2'
+    >
+      {showSuccess()}
+      {showError()}
+      {SignUpForm()}
     </Layout>
   );
 };
